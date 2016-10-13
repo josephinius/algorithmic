f2ff61b81a09e2969f77b09aa0a1fde1d1de6957
Jozef Genzor <jozef.genzor@gmail.com>
2016-10-13 18:18:09 +0200

    Update sort.py

--- sorting/sort.py
+++ sorting/sort.py
@@ -28,6 +28,43 @@ def insertion_sort(list_to_sort):
         list_to_sort[j+1] = mem
     return list_to_sort
     
+def merge_sort(list_to_sort): 
+    """
+    Sorts list of numbers using divide-and-conquer paradigm.
+    
+    :param list_to_sort: list to sort
+    :return: sorted list
+    """
+    n = len(list_to_sort)
+    if n == 1:
+        return list_to_sort
+    left = merge_sort(list_to_sort[:mid])
+    right = merge_sort(list_to_sort[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    """
+    Subroutine used in merge_sort. 
+    
+    :param left: left array
+    :param right: right array
+    :return: merged array
+    """
+    merged = []
+    l, r = 0, 0
+    while l < len(left) and r < len(right):
+        if left[l] < right[r]:
+            merged.append(left[l])
+            l += 1
+        else:
+            merged.append(right[r])
+            r += 1 
+    if l < len(left):
+        merged.extend(left[l:])
+    if r < len(right):
+        merged.extend(right[r:])
+    return merged 
+    
 def is_sorted(sorting_list):
     """Checks if list is sorted"""
     if len(sorting_list) == 1:
