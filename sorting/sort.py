f0e1e00f7e8c4bfdcfa0e6b5eb77f6e8cd24ca10
Jozef Genzor <jozef.genzor@gmail.com>
2016-10-13 17:47:02 +0200

    Update sort.py

--- sorting/sort.py
+++ sorting/sort.py
@@ -12,7 +12,22 @@ def shuffle_sort(list_to_sort):
         random.shuffle(list_to_sort)
     return list_to_sort
 
-
+def insertion_sort(list_to_sort):
+    """
+    Sorts list of numbers in Theta(n^2) time.
+    
+    :param list_to_sort: list to sort
+    :return: sorted list
+    """
+    for i in range(len(list_to_sort)):
+        mem = list_to_sort[i]
+        j = i-1
+        while j>=0 and list_to_sort[j] > mem:
+            list_to_sort[j+1] = list_to_sort[j]
+            j -= 1
+        list_to_sort[j+1] = mem
+    return list_to_sort
+    
 def is_sorted(sorting_list):
     """Checks if list is sorted"""
     if len(sorting_list) == 1:
